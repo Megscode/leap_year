a0dd77db17914ed4772d760f4aba8f59ca3bf241
Megan Hayes <megan.hayes@hotmail.co.uk>
2020-06-15 10:59:12 +0100

    year 1988 returns true

--- lib/leap_year.rb
+++ lib/leap_year.rb
@@ -1,6 +1,6 @@
 
   def leap_year?(n)
-    if n ==  2000
+    if n ==  2000 || n == 1988
       true
     else 
       false
--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -9,4 +9,8 @@ describe "leap years" do
   it "should return false when given 2001" do
     expect(leap_year?(2001)).to eq false
   end
+
+  it "should return true when given 1988" do
+    expect(leap_year?(1988)).to eq true
+  end 
 end
